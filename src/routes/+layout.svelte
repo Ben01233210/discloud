394aa92378a72919fe87bd35d24bef376eee9e78
Ben <72450926+Ben01233210@users.noreply.github.com>
2024-01-31 12:53:12 +0000

    designed nav and the start page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+	import "../app.css"
+</script>
+<nav class="flex justify-center">
+	<a href="/" class="font-bold">Discloud</a>
+	<a href="/" class="text-blue-500 hover:text-blue-700">Home</a>
+	<a href="/about" class="text-blue-500 hover:text-blue-700">About</a>
+	<a href="/tutorial" class="text-blue-500 hover:text-blue-700">Tutorial</a>
+</nav>
+<slot></slot>
+
+<style>
+	nav {
+		@apply shadow-sm bg-white py-4 px-8;
+	}
+	nav a {
+		@apply mx-4;
+	}
+	
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://google.com">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    let webhook = "";
+    let login = async () => {
+        let data = await fetch("https://api.discloud.us/login", {
+            method: "POST",
+            body: JSON.stringify({
+                webhook: webhook
+            })
+        })
+    }
+</script>
+
+
+<div class="flex flex-col m-8 p-8 bg-gray-100 rounded-lg shadow-lg gap-2">
+    <h1 class="font-bold text-3xl self-center">Welcome to Discloud</h1>
+    <p>Discloud is a free cloud storage platform that allows you to securely store and access your files from anywhere. With Discloud, you can easily upload, organize, and share your documents, photos, videos, and more. Experience the convenience and flexibility of cloud storage with Discloud.</p>
+    <a href="/tutorial" class="bg-blue-500 hover:bg-blue-700 text-white 
+    font-bold py-2 px-4 rounded self-center w-min">Tutorial</a>
+    <input bind:value={webhook} class="border-2 border-gray-300 rounded-lg p-2 w-2/4 self-center" type="text" placeholder="Webhook">
+    <button on:click={login} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded self-center w-min">Login</button>
+</div>
