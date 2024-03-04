8269f8945729d2be2c3ec929c5674326813da1c2
Ben <ben.blankenburg@icloud.com>
2024-03-04 09:02:53 +0000

    added arvids code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,24 @@
 <script>
-	import "../app.css"
+    import "../app.css";
 </script>
-<nav class="flex justify-center">
-	<a href="/" class="font-bold">Discloud</a>
-	<a href="/" class="text-blue-500 hover:text-blue-700">Home</a>
-	<a href="/about" class="text-blue-500 hover:text-blue-700">About</a>
-	<a href="/tutorial" class="text-blue-500 hover:text-blue-700">Tutorial</a>
-</nav>
-<slot></slot>
 
-<style>
-	nav {
-		@apply shadow-sm bg-white py-4 px-8;
-	}
-	nav a {
-		@apply mx-4;
-	}
-	
-</style>
+<div class="bg-background">
+    <div class="flex justify-center text-foreground py-2 border-b gap-2">
+        <a href="/" class="font-bold">Discloud</a>
+        <a href="/" class="text-primary hover:secondary">Home</a>
+        <a href="/about" class="text-blue-500 hover:text-blue-700">About</a>
+        <a href="/tutorial" class="text-blue-500 hover:text-blue-700"
+            >Tutorial</a
+        >
+    </div>
+    <slot />
+
+    <style>
+        nav {
+            @apply shadow-sm bg-white py-4 px-8;
+        }
+        nav a {
+            @apply mx-4;
+        }
+    </style>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,201 @@
 <script lang="ts">
-    let webhook = "";
-    let login = async () => {
-        let data = await fetch("https://api.discloud.us/login", {
-            method: "POST",
-            body: JSON.stringify({
-                webhook: webhook
-            })
-        })
+    import { Button } from "$lib/components/ui/button";
+
+    import * as Card from "$lib/components/ui/card";
+
+    import {onMount} from 'svelte';
+
+
+    const DATABASE_FILE_NAME = "discloud-lock.txt";
+    let databaseFileId: string;
+
+    let authorizeButtonVisible = false;
+    let signoutButtonVisible = false;
+
+    const CLIENT_ID = "410214321825-o40h4kkrj2j6cnuguu88qn4i3f7ve5fg.apps.googleusercontent.com";
+    const API_KEY = "AIzaSyAieD4rFf6p0GTutU8VGB1uT4PBX7RHWL4";
+    const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest';
+    const SCOPES = 'https://www.googleapis.com/auth/drive';
+
+    let tokenClient;
+    let gapiInited = false;
+    let gisInited = false;
+
+    onMount(() => {
+        loadScript('https://apis.google.com/js/api.js', gapiLoaded);
+        loadScript('https://accounts.google.com/gsi/client', gisLoaded);
+    });
+
+    function loadScript(src, callback) {
+        const script = document.createElement('script');
+        script.async = true;
+        script.defer = true;
+        script.src = src;
+        script.onload = callback;
+        document.body.appendChild(script);
+    }
+
+    /**
+     * Callback after api.js is loaded.
+     */
+    function gapiLoaded() {
+        gapi.load('client', initializeGapiClient);
+    }
+
+    /**
+     * Callback after the API client is loaded. Loads the
+     * discovery doc to initialize the API.
+     */
+    async function initializeGapiClient() {
+        await gapi.client.init({
+            apiKey: API_KEY,
+            discoveryDocs: [DISCOVERY_DOC],
+        });
+        gapiInited = true;
+        maybeEnableButtons();
+    }
+
+    /**
+     * Callback after Google Identity Services are loaded.
+     */
+    function gisLoaded() {
+        tokenClient = google.accounts.oauth2.initTokenClient({
+            client_id: CLIENT_ID,
+            scope: SCOPES,
+            callback: '', // defined later
+        });
+        gisInited = true;
+        maybeEnableButtons();
+    }
+
+    /**
+     * Enables user interaction after all libraries are loaded.
+     */
+    function maybeEnableButtons() {
+        if (gapiInited && gisInited) {
+            document.getElementById('authorize_button').style.visibility = 'visible';
+        }
+    }
+
+    /**
+     *  Sign in the user upon button click.
+     */
+    function handleAuthClick() {
+        tokenClient.callback = async (resp) => {
+            if (resp.error !== undefined) {
+                throw (resp);
+            }
+            document.getElementById('signout_button').style.visibility = 'visible';
+            document.getElementById('authorize_button').innerText = 'Refresh';
+            await initDatabase();
+        };
+
+        if (gapi.client.getToken() === null) {
+            // Prompt the user to select a Google Account and ask for consent to share their data
+            // when establishing a new session.
+            tokenClient.requestAccessToken({prompt: 'consent'});
+        } else {
+            // Skip display of account chooser and consent dialog for an existing session.
+            tokenClient.requestAccessToken({prompt: ''});
+        }
+    }
+
+    /**
+     *  Sign out the user upon button click.
+     */
+    function handleSignoutClick() {
+        const token = gapi.client.getToken();
+        if (token !== null) {
+            google.accounts.oauth2.revoke(token.access_token);
+            gapi.client.setToken(null);
+            document.getElementById('authorize_button').innerText = 'Authorize';
+            document.getElementById('signout_button').style.visibility = 'hidden';
+        }
+    }
+
+    async function initDatabase() {
+        const fileId = await fileExists(DATABASE_FILE_NAME);
+
+        if (fileId) {
+            databaseFileId = fileId;
+            console.log("found database");
+        } else {
+            databaseFileId = await createDatabaseFile();
+            console.log("created new database");
+        }
+
+        console.log("initialized database");
+    }
+
+    async function createDatabaseFile(): Promise<string> {
+        const response = await gapi.client.drive.files.create({
+            resource: {
+                "name": DATABASE_FILE_NAME,
+                "mimeType": "text/plain"
+            }
+        });
+
+        return response.result.id;
+    }
+
+    async function fileExists(fileName: string): Promise<string | null> {
+        const response = await gapi.client.drive.files.list({
+            "q": `name='${fileName}' and trashed=false`,
+            "spaces": "drive",
+            "fields": "files(id)"
+        });
+        const files = response.result.files;
+
+        if (files && files.length > 0) {
+            return files[0].id;
+        } else {
+            return null;
+        }
+    }
+
+    export async function updateDatabaseContent(content: string) {
+        try {
+            return await gapi.client.request({
+                path: `/upload/drive/v3/files/${databaseFileId}`,
+                method: "PATCH",
+                params: {
+                    uploadType: "media"
+                },
+                body: content
+            });
+        } catch (error) {
+            console.error(error);
+            return null;
+        }
+    }
+
+    export async function getDatabaseContent() {
+        try {
+            const response = await gapi.client.drive.files.get({
+                fileId: databaseFileId,
+                alt: "media"
+            });
+
+            return response.body;
+        } catch (error) {
+            console.error(error);
+            return null;
+        }
     }
 </script>
 
-
-<div class="flex flex-col m-8 p-8 bg-gray-100 rounded-lg shadow-lg gap-2">
-    <h1 class="font-bold text-3xl self-center">Welcome to Discloud</h1>
-    <p>Discloud is a free cloud storage platform that allows you to securely store and access your files from anywhere. With Discloud, you can easily upload, organize, and share your documents, photos, videos, and more. Experience the convenience and flexibility of cloud storage with Discloud.</p>
-    <a href="/tutorial" class="bg-blue-500 hover:bg-blue-700 text-white 
-    font-bold py-2 px-4 rounded self-center w-min">Tutorial</a>
-    <input bind:value={webhook} class="border-2 border-gray-300 rounded-lg p-2 w-2/4 self-center" type="text" placeholder="Webhook">
-    <button on:click={login} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded self-center w-min">Login</button>
+<div class="w-full h-full text-foreground">
+   <Card.Root class="flex flex-col gap-2 m-4 p-4">
+        <Card.Title class="text-center">Discloud</Card.Title>
+        <p>
+            Discloud is a free cloud storage platform that allows you to
+            securely store and access your files from anywhere. With Discloud,
+            you can easily upload, organize, and share your documents, photos,
+            videos, and more. Experience the convenience and flexibility of
+            cloud storage with Discloud.
+        </p>
+        <Button on:click={handleAuthClick} >Sign up</Button>
+        <Button href="/login">Login</Button>
+        <button on:click={handleAuthClick} style:visibility={authorizeButtonVisible ? 'visible' : 'hidden'}>Sign up</button>
+    </Card.Root>
 </div>
